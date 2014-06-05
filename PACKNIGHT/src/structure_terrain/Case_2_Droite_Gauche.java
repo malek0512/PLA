1911519c3f7dbcbdd4588fd0e6c430f0ccc71543
Quego <quentinmace@hotmail.com>
2014-06-05 15:15:31 +0200

    ajout attribut case 2/4

--- PACKNIGHT/src/structure_terrain/Case_2_Droite_Gauche.java
+++ PACKNIGHT/src/structure_terrain/Case_2_Droite_Gauche.java
@@ -2,4 +2,20 @@ package structure_terrain;
 
 public class Case_2_Droite_Gauche extends Case_2 {
 
+	private Case Droite;
+	private Case Gauche;
+	
+	
+	@Override
+	protected void deplacer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void informer() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- PACKNIGHT/src/structure_terrain/Case_2_Haut_Gauche.java
+++ PACKNIGHT/src/structure_terrain/Case_2_Haut_Gauche.java
@@ -1,5 +1,20 @@
 package structure_terrain;
 
 public class Case_2_Haut_Gauche extends Case_2{
+	
+	private Case Haut;
+	private Case Gauche;
+
+	@Override
+	protected void deplacer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void informer() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
