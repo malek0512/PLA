523ae3acccd6e9a6d33b99a3e219ddaa8591714d
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2014-06-06 16:29:45 +0200

    Ajout du package test et de la classe test_terrain

---
+++ PACKNIGHT/src/Tests/test_terrain.java
@@ -0,0 +1,39 @@
+package Tests;
+
+import structure_terrain.Case;
+import structure_terrain.Terrain;
+
+public class test_terrain {
+
+	public static void main(String[] args) {
+		int j;
+		Terrain terr=new Terrain(5,7);
+		//setCase(int ligne, int colonne, Case ca)
+		
+		Case mur=new Case(false);
+		Case chemin=new Case(true);
+		
+		for(j=0;j<5;j++){
+			terr.setCase(j, 0, mur);
+			terr.setCase(j, 6, mur);
+		}
+		for(j=0;j<5;j++){
+			terr.setCase(j, 1, chemin);
+			terr.setCase(j, 5, chemin);
+		}
+		
+		for(j=0;j<5;j++){
+			terr.setCase(j, 2,chemin);
+			terr.setCase(j, 4, chemin);
+		}
+		terr.setCase(0, 3, chemin);
+		terr.setCase(1, 3, mur);
+		terr.setCase(2, 3, mur);
+		terr.setCase(3, 3, mur);
+		terr.setCase(4, 3, chemin);
+		
+		terr.afficher();
+		
+	}
+
+}
