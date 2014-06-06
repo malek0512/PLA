cf33a1759fec128b2e338af1d3e9706f08b83229
Malek MAMMAR <malekmammar@gmail.com>
2014-06-06 15:21:31 +0200

    suppression bin. .stteing/ et .classpath

--- PACKNIGHT/src/automate/Ordonnanceur.java
+++ PACKNIGHT/src/automate/Ordonnanceur.java
@@ -1,5 +1,16 @@
 package automate;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import personnages.Personnage;
+
 public class Ordonnanceur {
+	List<Objet> fap;
+	List<Objet> processus;
 	
+	public Ordonnanceur() {
+		fap = new LinkedList<Objet>();
+		processus = new LinkedList<Objet>();
+	}
 }
