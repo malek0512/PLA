bcc91fa4fde7f02213a357bd9b86d48e9ee0fce0
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2014-06-06 15:19:49 +0200

    création ghost

--- PACKNIGHT/src/automate/Automate.java
+++ PACKNIGHT/src/automate/Automate.java
@@ -1,12 +1,13 @@
 package automate;
 
 import parser.Parser;
+import personnages.Personnage;
 
 public class Automate {
 
 	int tableTransition[][];
 	String tableSortie[][];
-	
+	Personnage personnage;
 	/*
 	 * Prend un fichier XML et remplie les attributs de l'automate
 	 */
