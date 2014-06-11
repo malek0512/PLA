f22743171fb85bae0bf23de0a8377ddca7d6f644
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2014-06-11 09:32:59 +0200

    ajout des primitives mur + pacman est dans croix + estIntersection

--- PACKNIGHT/src/controleur/automate/PrimitivesTest.java
+++ PACKNIGHT/src/controleur/automate/PrimitivesTest.java
@@ -52,4 +52,23 @@ public class PrimitivesTest extends Primitives {
 			return Automate.CASE_OCCUPEE;
 		}
 	}
+	public boolean estIntersection(Coordonnees coord){
+		int n=0;
+		Coordonnees tmp=coord;
+		if(Personnage.getTerrain().getCase(tmp.x+1,tmp.y).isAccessable()){
+			n++;
+		}
+		if(Personnage.getTerrain().getCase(tmp.x-1,tmp.y).isAccessable()){
+			n++;
+		}
+		if(Personnage.getTerrain().getCase(tmp.x,tmp.y+1).isAccessable()){
+			n++;
+		}
+		if(Personnage.getTerrain().getCase(tmp.x,tmp.y+1).isAccessable()){
+			n++;
+		}
+		return n>2;
+		
+	}
+	
 }
