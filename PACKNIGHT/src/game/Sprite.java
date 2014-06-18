76ad19f7ccb068db3561f7cd3abbb1f61f176358
Quego <quentinmace@hotmail.com>
2014-06-18 15:24:12 +0200

    decomposition Window Game

---
+++ PACKNIGHT/src/game/Sprite.java
@@ -0,0 +1,29 @@
+package game;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SpriteSheet;
+
+public class Sprite {
+	
+	private static Animation loadAnimation(SpriteSheet spriteSheet, int startX, int endX, int y) {
+	    Animation animation = new Animation();
+	    for (int x = startX; x < endX; x++) {
+	        animation.addFrame(spriteSheet.getSprite(x, y), 100);
+	    }
+	    return animation;
+	}
+	
+	public static void toSprite(Animation animation[],SpriteSheet Personnage){
+		
+	    animation[0] = loadAnimation(Personnage, 0, 1, 0);
+	    animation[1] = loadAnimation(Personnage, 0, 1, 1);
+	    animation[2] = loadAnimation(Personnage, 0, 1, 2);
+	    animation[3] = loadAnimation(Personnage, 0, 1, 3);
+	    animation[4] = loadAnimation(Personnage, 1, 9, 0);
+	    animation[5] = loadAnimation(Personnage, 1, 9, 1);
+	    animation[6] = loadAnimation(Personnage, 1, 9, 2);
+	    animation[7] = loadAnimation(Personnage, 1, 9, 3);
+	    
+		}
+
+}
