50f4bf9d2b9c7b6173b063b85e4f9063cb9edfe3
Quego <quentinmace@hotmail.com>
2014-06-19 14:13:23 +0200

    dead scree and win screen

---
+++ PACKNIGHT/src/game/Dead.java
@@ -0,0 +1,61 @@
+package game;
+
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+public class Dead extends BasicGameState {
+	   public static final int ID = 6;
+	   private Image PAUSE;
+	   private StateBasedGame game;
+
+	   
+	   public int getID() {
+	      return ID;
+	   }
+
+	   public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		   	this.game = game;
+
+	   }
+
+	   public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		      PAUSE = new Image("src/graphisme/main/ressources/map/image/Pause.jpeg");
+		      PAUSE.draw(0,0);
+		      g.setColor(Color.white);
+		      g.drawString("You dead", 250, 100);
+		      g.drawString("Main Menu (SPACE)", 250, 150);
+
+
+	   }
+
+	   
+	   public void update(GameContainer container, StateBasedGame game, int delta) {
+	   }
+	   
+	   public void keyReleased(int key, char c) {
+	      switch (key) {
+	      		case Input.KEY_SPACE: game.enterState(Choix.ID, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));break;
+	      		case Input.KEY_M: if(WindowGame.M.playing()) WindowGame.M.pause() ;else WindowGame.M.resume(); break;
+	      		case Input.KEY_ESCAPE:Menu.container.exit(); break;
+
+	      }
+	   }
+
+
+
+	public static void Pause_Game(Graphics g, Image PAUSE_IMAGE)
+	{
+
+
+	}
+
+}
