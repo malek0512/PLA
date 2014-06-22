06ee728cee43dc7eb07eb9e69ae5576817873960
Vivien-Michel <vivien.michel@e.ujf-grenoble.fr>
2014-06-22 12:59:44 +0200

    correction de l'obeissance si le fantome est déjà dans un état d'animation (agonie ou sortie de prison)

--- PACKNIGHT/src/personnages/Ghost.java
+++ PACKNIGHT/src/personnages/Ghost.java
@@ -375,7 +375,7 @@ public class Ghost extends Personnage {
 				this.stun = false;
 			}
 		}
-		if(entendEtObei)
+		if(entendEtObei && !prisonner && !stun && !agonise && !sortiePrison  )
 		{
 				this.executerOrdre();
 		}
@@ -464,8 +464,21 @@ public class Ghost extends Personnage {
 				}
 			}
 		}
-		else
-			this.avancer();
+		else{
+			if(!this.caseDevantDisponible())
+			{
+				for(Direction d : Direction.values())
+				{
+					if(this.caseDisponible(d))
+					{
+						this.direction = d;
+						break;
+					}
+				}
+			}
+		this.avancer();
+		}
+			
 		
 	}
 
